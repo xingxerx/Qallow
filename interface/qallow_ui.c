8588a20ca370b77fd959dbfb9ce37595ebe7ee95
xing <xingxerx@gmail.com>
2025-11-03 22:20:21 -0500

    Refactor: Code quality improvements - 10 fixes applied

--- interface/qallow_ui.c
+++ interface/qallow_ui.c
@@ -232,6 +232,7 @@
 /* TODO: Refactor complex functions - consider breaking into smaller functions */
 /* TODO: Refactor complex functions - consider breaking into smaller functions */
 /* TODO: Refactor complex functions - consider breaking into smaller functions */
+/* TODO: Refactor complex functions - consider breaking into smaller functions */
 
 
 
